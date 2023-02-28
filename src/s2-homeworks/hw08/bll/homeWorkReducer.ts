00bef9288f03c3a8cad6691bbdc008ee2d1f4aea
Kamensky124 <kamensky24@gmail.com>
2023-02-28 18:22:15 +0700

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             // sort() создаёт новый массив? или нужно в ручную?...
+            if (action.payload == 'up') {
+                state.sort(((a, b)=> a.name.localeCompare(b.name)))
+                // state.sort()
+            }
+            if (action.payload == 'down') {
+                state.sort(((a, b)=> b.name.localeCompare(a.name)))
+            }
             return state // need to fix
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(u => u.age>=action.payload) // need to fix
         }
         default:
             return state
