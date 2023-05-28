c1814e27aeaa740184cac907935f930e482a2878
Kamensky124 <kamensky24@gmail.com>
2023-05-28 13:48:56 +0700

    remember

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,30 @@
+import {Simulate} from "react-dom/test-utils";
+import change = Simulate.change;
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeIdType): typeof state => { // fix any
+
+    console.log(typeof state)
+    debugger
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID':
+            return {
+                ...state,
+                themeId: action.id
+            }
+
 
         default:
             return state
     }
+}
+
+type changeThemeIdType = {
+    type: 'SET_THEME_ID',
+    id: number
 }
 
 export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
